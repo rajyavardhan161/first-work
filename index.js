bdc138faf821dd383f157dd05e899416270661ad
rajyavardhan <rajyavardhans805>
2025-05-20 14:36:55 +0530

    first work

---
+++ index.js
@@ -0,0 +1,38 @@
+// let first=document.getElementById('parent').firstElementChild;
+    // console.log(first);
+    // first.style="background:red";
+        // --------------------------------------------------
+
+    // let first=document.getElementById('parent').lastElementChild;
+    // console.log(first);
+    // first.style="background:red";
+        // --------------------------------------------------
+
+    // let first=document.getElementById('three').previousElementSibling;
+    // console.log(first);
+    // first.style="background:red";
+        // ---------------------------------------------------
+
+
+    // let first=document.getElementById('three').nextElementSibling;
+    // console.log(first);
+    // first.style="background:red";
+    //    ---------------------------------------------------
+
+    let first=document.getElementById('parent').children[2];
+    console.log(first);
+    first.style="background:red";
+        // ---------------------------------------------------
+
+        // let first = document.getElementById('parent').children[1];
+        // console.log(first);
+        // first.style = "background:yellow"
+        // ----------------------------------------------------------
+
+    // let first=document.getElementById("one").parentElement;
+    // first.style="background:yellow"
+    // ----------------------------------------------------------------
+
+        // let first = document.getElementById("one").parentElement;
+        // first.style = "background:lightpink";
+        // -------------------------------------------------
